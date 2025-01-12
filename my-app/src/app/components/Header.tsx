47e3c482a023791e749ffe84decdbbc5000a2e1c
naghmana1220 <naghmanaasif85@gmail.com>
2025-01-12 19:28:28 +0500

    first commit

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,93 @@
+"use client"
+import { Search, Menu, ShoppingCart, Heart,User } from "lucide-react";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+const Header = () => {
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false)
+   
+  return (
+    <header className="w-full mx-auto bg-white sticky top-0 font-Poppins border-b-[1px] border-black z-20 shadow-xl">
+        
+
+      <div className="max-w-[1240px] mx-auto h-[80px] flex items-center justify-between p-3">
+      
+          <Image src={"/images/Logo1.png"} alt="Logo" width={100} height={100} className="w-16 h-auto lg:ml-10" />
+
+
+            <span className="ml-3 text-3xl font-['Montserrat'] font-bold  text-[#000000]  ">Furniro</span>
+        {/* Navbar */}
+        <nav className="font-medium flex flex-1 justify-center">
+    
+        <ul className="hidden sm:flex gap-10">
+            <li className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">
+            <Link href="/shop">Shop</Link>
+            </li>
+            <li className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">
+            <Link href="/blog">Blog</Link>
+            
+            </li>
+            <li className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">
+            <Link href="/contact">Contact</Link>
+            </li>
+        </ul>
+        </nav>
+
+        {/* Icons */}
+        <div className="flex items-center gap-3">
+        <Link href={"/account"}><User className="hover:bg-[#f0d786] p-[6px] rounded" size={37}/></Link>
+          <Link href={"/"}><Search className=" p-[6px] rounded" size={37} /></Link>
+          <Link href={"/"}><Heart className=" p-[6px] rounded" size={37}/></Link>
+          <Link href={"/"}><ShoppingCart className=" p-[6px] rounded" size={37} /></Link>
+          <Menu className="sm:hidden cursor-pointer" onClick={()=> setIsNavbarOpen(true)} size={25}/>
+        </div>
+
+      </div>
+
+      {/* Mobile Header */}
+      <Sheet open={isNavbarOpen} onOpenChange={setIsNavbarOpen}>
+        <SheetContent className="bg-white w-[15rem]" side={"left"}>
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+            <SheetDescription>
+              <ul className="flex flex-col gap-10">
+                <li>
+                  <Link href="/" className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">Home</Link>
+                </li>
+                <li>
+                  <Link href="/shop" className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">Shop</Link>
+                </li>
+                <li>
+                  <Link href="/blog" className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">Blog</Link>
+
+                </li>
+                <li>
+                  <Link href="/contact" className="cursor-pointer hover:border-b-[#000000] hover:border-b-[0.5px] transition-all duration-300">Contact</Link>
+                </li>
+              </ul>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>      
+
+     
+                
+    </header>
+  )
+}
+
+export default Header
---
+++ my-app/src/app/components/Hero.tsx
@@ -0,0 +1,58 @@
+"use client"
+ import Image from "next/image";
+
+ const Hero = () => {
+     return(
+        <div className="max-w-[1240px]">
+ <div className="relative  h-[790px] overflow-hidden sm:w-auto w-full lg:w-[1900px] ">
+   
+            <Image src={"/images/Hero1.png"} alt="Hero" width={1440} height={100} className="object-cover w-full h-full" />
+
+            <div className="absolute top-4 right-4 bg-[#FFF3E3]   p-4 w-[643px] h-[443px] pl-[100px]   pt-[100px] mt-[1px]  rounded-xl max-w-xs justify-center items-center">
+              
+                <h4 className=" font-semibold text-[#333333] ">New Arrival</h4>
+              
+                <h1 className="text-[#BB8E2F] mt-2 text-2xl  font-['Poppins']  items-center justify-center pr-10  font-bold">
+                Discover Our <br /> New Collection
+                </h1>
+                <p className=" font-medium  text-[#333333] leading-6">Lorem, ipsum dolor si elit. Explicabo mollitia recusait ipsam in porroo.</p>
+                <button className="mt-3 bg-[#B88E2F] text-white  px-4 py-2  ">
+                   Buy Now
+                </button>
+            </div>
+
+</div>
+            <div className="lg:w-[1900px]">
+             <div className="container mx-auto p-6">
+            <h1 className="text-3xl font-bold text-center  mb-4">Browse The Range</h1>
+            <p className="text-center mb-8 max-w-xl mx-auto">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                <div className="overflow-hidden rounded-lg shadow-lg">
+                 <Image src="/images/Hero2.png" alt="Image 1" width={300} height={480} className="w-full h-auto" /> 
+                 Dining
+                </div>
+                <div className="overflow-hidden rounded-lg shadow-lg">
+                    <Image src="/images/Hero3.png" alt="Image 2"  width={381} height={480} className="w-full h-auto" />
+                    Living
+                </div>
+
+
+ <div className="overflow-hidden rounded-lg shadow-lg items-center justify-center">
+                    <Image src="/images/Hero4.png" alt="Image 3" width={381} height={480}  className="w-full h-auto" />
+                    Bedroom
+                </div>
+            </div>
+        </div>
+            </div>
+
+    
+     
+       
+  
+  
+  </div>
+        );
+};
+export default Hero 
